a779ffce1d1aa79bc3634fc440109aec4680e247
Moonsight <aidanali65@gmail.com>
2023-10-23 23:43:35 -0400

    2nd Commit Ending Off Night 10/23/2023 Going well so far Youtube Video by "GreatStack" has helped me out quite a bit an i'm feeling freshed to learn react.js again. hopefully i will be able to catch up and move onto building a next13 website But yes Ending off day one

---
+++ src/Component/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Hero.css'
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <div className="hero-left">sdfsdf</div>
+      <div className="hero-right">234234</div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/Pages/Shop.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Hero from '../Component/Hero/Hero'
+const Shop = () => {
+  return (
+    <div>
+        <Hero/>
+    </div>
+  )
+}
+
+export default Shop
